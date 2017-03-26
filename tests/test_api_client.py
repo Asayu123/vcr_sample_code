25de549db2d81c31cdeaf587d08c4ebc5dbea7cc
Asayu123 <Asayu123@users.noreply.github.com>
2017-03-26 22:40:43 +0900

    Test: Activate VCR and changed cassette path

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -6,7 +6,7 @@ from bin.sample_api_client import SampleApiClient
 # Instantiate VCR in order to Use VCR in test scenario.
 
 vcr_instance = vcr.VCR(  # Following option is often used.
-    cassette_library_dir='vcr_sample_code/tests/vcr/cassettes/',  # A Location to storing VCR Cassettes
+    cassette_library_dir='tests/vcr/cassettes/',  # A Location to storing VCR Cassettes
     decode_compressed_response=True,  # Store VCR content (HTTP Requests / Responses) as a Plain text.
     serializer='json',  # Store VCR Record as a JSON Data
 )
@@ -15,7 +15,7 @@ vcr_instance = vcr.VCR(  # Following option is often used.
 
 
 class TestApiClient(TestCase):
-
+        @vcr_instance.use_cassette
         def test_todo_filter(self):
 
             client = SampleApiClient(url='jsonplaceholder.typicode.com')
