38bc0e46fc75c0acff5fb1abf60cf8c1c2088b1e
Asayu123 <Asayu123@users.noreply.github.com>
2017-03-26 23:26:03 +0900

    Test: Chaged VCR cassette path.

--- tests/test_sample_api_client_with_vcr.py
+++ tests/test_sample_api_client_with_vcr.py
@@ -5,7 +5,7 @@ from bin.sample_api_client import SampleApiClient
 # Instantiate VCR in order to Use VCR in test scenario.
 
 vcr_instance = vcr.VCR(  # Following option is often used.
-    cassette_library_dir='tests/vcr/cassettes/',  # A Location to storing VCR Cassettes
+    cassette_library_dir='vcr/cassettes/',  # A Location to storing VCR Cassettes
     decode_compressed_response=True,  # Store VCR content (HTTP Requests / Responses) as a Plain text.
     serializer='json',  # Store VCR Record as a JSON Data
 )
